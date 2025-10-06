aa3737bbd393b51a65c9d2e601638051a2750367
mginberg <111248279+mginberg@users.noreply.github.com>
2025-10-06 08:30:16 -0400

    proxy.py

--- proxy.py
+++ proxy.py
@@ -3,24 +3,18 @@ from fastapi.middleware.cors import CORSMiddleware
 import requests
 
 app = FastAPI()
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"], allow_credentials=True, allow_methods=["*"], allow_headers=["*"]
-)
+app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
 
 API_ENDPOINT = "https://tabakattorneys.lawruler.com/api-legalcrmapp.aspx"
 API_KEY = "8A2A55F85D784406B7F79DC286745"
 
 @app.get("/lead_status")
-def get_status(lead_id: str):
-    url = API_ENDPOINT
+def lead_status(lead_id: str):
     params = {
         "Key": API_KEY,
         "Operation": "GetStatus",
         "ReturnXML": "True",
         "LeadId": lead_id
     }
-    resp = requests.get(url, params=params, timeout=15)
-    return {"status_code": resp.status_code, "api_url": resp.url, "result": resp.text}
-
+    resp = requests.get(API_ENDPOINT, params=params, timeout=20)
+    return {"status_code": resp.status_code, "result": resp.text, "api_url": resp.url}
